fd6257b6d2b8a6274da563230c112fcfdefb2302
Kenatho <89846381+Kenatho@users.noreply.github.com>
2024-11-01 15:16:32 +0100

    Added comments to explain the logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 
 exports.registerUser = async (req, res) => {
-    const { username, password } =req.body;
+    const { username, password } = req.body;
     try {
       let user = await User.findOne({ username });
       if (user) return res.status(400).json({ message: "User already exists" });
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 require('dotenv').config();
 const express = require('express');
-const connectDB = require('./config/db');
+const connectDB = require('./config/db'); //The database file for MongoDB connection
 const cors = require('cors');
 
-const app = express();
-connectDB();
 
+const app = express();
+connectDB(); //Connecting to MongoDB    
+
+//Middleware
 app.use(cors());
 app.use(express.json());
 
+//Routes
 app.use('/api/auth', require('./routes/auth'));
 
+//Server Connection
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))

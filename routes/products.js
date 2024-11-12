8e697822a17271795e42ea165f4a49c98c77bffd
Kenatho <89846381+Kenatho@users.noreply.github.com>
2024-11-12 14:31:52 +0100

    Configured API for products

--- routes/products.js
+++ routes/products.js
@@ -7,49 +7,49 @@ const products = [
     id: 1,
     title: 'Modern Sofa',
     price: 499.99,
-    image: '/images/sofa.jpeg'
+    image: 'http://localhost:5000/images/sofa.jpeg'
   },
   {
     id: 2,
     title: 'Wooden Dining Table',
     price: 299.99,
-    image: '/images/table.jpg'
+    image: 'http://localhost:5000/images/table.jpg'
   },
   {
     id: 3,
     title: 'Wooden Kitchen Chair',
     price: 99.99,
-    image: '/images/chair.jpg'
+    image: 'http://localhost:5000/images/chair.jpg'
   },
   {
     id: 4,
     title: 'Tribal Pattern Carpet',
     price: 49.99,
-    image: '/images/carpet.jpg'
+    image: 'http://localhost:5000/images/carpet.jpg'
   },
   {
     id: 5,
     title: 'Modern Styled Lamp',
     price: 79.99,
-    image: '/images/lamp.jpg'
+    image: 'http://localhost:5000/images/lamp.jpg'
   },
   {
     id: 6,
     title: 'Humidifier',
     price: 99.99,
-    image: '/images/humi.jpg'
+    image: 'http://localhost:5000/images/humi.jpg'
   },
   {
     id: 7,
     title: 'Modern Lamp',
     price: 89.99,
-    image: '/images/lamp.jpg'
+    image: 'http://localhost:5000/images/lamp.jpg'
   },
   {
     id: 8,
     title: 'Living Room Table',
     price: 99.99,
-    image: '/images/roomtable.jpg'
+    image: 'http://localhost:5000/images/roomtable.jpg'
   }
 ];
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db'); //The database file for MongoDB connection
 const cors = require('cors');
+const path = require('path');
 
 
 const app = express();
@@ -10,11 +11,13 @@ connectDB(); //Connecting to MongoDB
 //Middleware
 app.use(cors());
 app.use(express.json());
-app.use('/images', express.static('/images'))
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 //Routes
+app.use(cors({origin: 'http://localhost:6006',}));
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/products'));
+
 
 //Server Connection
 const PORT = process.env.PORT || 5000;
